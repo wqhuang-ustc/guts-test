bf7c5bc24c24f93ff46c7d1099151b93d61262eb
weiqinghuang <weiqing.huang@adtoox.com>
2021-08-05 15:12:40 +0200

    cleanup the code

--- app_dockerfile/api.py
+++ app_dockerfile/api.py
@@ -43,26 +43,6 @@ db = mongodb_client.db
 
 jwt = JWT(app, authenticate, identity)
 
-# Create sales test data here to simulate the result from Mongodb
-
-sales_records = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Demo for GUTS test</h1><p>This site is a prototype API for showing sales data</p>"
@@ -75,10 +55,6 @@ def protected():
 @app.route('/api/v1/sales/all')
 @jwt_required()
 def api_all():
-    bson_sales = db.sales.find_one()
-    # convert the record to json
-    sales = dumps(bson_sales)
-    
     sales_all = []
     bson_sales_all = db.sales.find()
     for sale in bson_sales_all:
